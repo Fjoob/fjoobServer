97b98922d450c36d2418ad81b3ee171a50d1f7c7
piemree <pi.emree@gmail.com>
2021-12-18 11:06:57 +0300

    Create project post route validation

--- v1/src/api-routes/Projects.js
+++ v1/src/api-routes/Projects.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const router=express.Router()
-const {create,index} = require('../controller/Project');
+const express = require("express");
+const router = express.Router();
+const { create, index } = require("../controller/Project");
+const { createValidation } = require("../validations/Project");
+const {validate} = require("../middlewares/validate")
 
-router.get('/', index)
-router.post('/', create)
+router.get("/", index);
 
-module.exports = router
+router.route("/").post(validate(createValidation), create);
+
+module.exports = router;
